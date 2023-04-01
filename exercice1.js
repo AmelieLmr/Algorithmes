90e62454aa64a5a53d42c61fddcbce1542444a6b
Lyz59 <amelie_du89@hotmail.fr>
2023-04-01 11:19:04 +0200

    first commit

---
+++ exercice1.js
@@ -0,0 +1,6 @@
+let nombreDemander = prompt("Entrer un nombre :");
+
+while(nombreDemander != 10){
+    nombreDemander = prompt("Je veux un 10 !");
+}
+alert("Merci c'est tout ce que je voulais !");
